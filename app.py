ed3c61b586b80bbdc27ac88d877fd67d55f242b8
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-02-02 14:28:32 +0100

    Extract scripts module

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import string
 import toolforge
 import yaml
 
+import scripts
 import unicodescripts
 
 
@@ -208,7 +209,7 @@ def any_diff():
                                           torelative='prev',
                                           prop=['diff'],
                                           formatversion=2)['compare']['body']
-            script = primary_script_of_diff(diff_body)
+            script = scripts.primary_script_of_diff(diff_body)
             if script is not None and script not in supported_scripts:
                 continue
         return flask.redirect(flask.url_for('diff', id=id))
@@ -296,42 +297,13 @@ def fix_markup(html):
             link['href'] = 'https://www.wikidata.org' + href
     return flask.Markup(str(soup))
 
-def primary_script_of_diff(html):
-    soup = bs4.BeautifulSoup(html, 'html.parser')
-    elements = [content for content in soup.contents if type(content) is bs4.Tag]
-    texts = []
-    for i in range(0, len(elements), 2):
-        lineno = elements[i].get_text()
-        if (lineno.startswith('label / ') or
-            lineno.startswith('description /') or
-            lineno.startswith('aliases /') or
-            lineno.startswith('links /')):
-            texts += (element.get_text() for element in elements[i+1].select('.diff-addedline, .diff-deletedline'))
-        elif lineno.startswith('Property /'):
-            texts += (element.get_text() for element in elements[i+1].select('.wb-monolingualtext-value'))
-            texts += (element.get_text() for element in elements[i+1].select('a.extiw[href^="//commons.wikimedia.org/"]'))
-    scripts = scripts_of_text(char for text in texts for char in text)
-    if scripts:
-        return scripts[0]
-    else:
-        return None
-
-def scripts_of_text(text):
-    scripts = {}
-    for char in text:
-        script = unicodescripts.script(char)
-        if script not in {'Common', 'Inherited', 'Unknown'}:
-            scripts[script] = scripts.get(script, 0) + 1
-    common_scripts = sorted(scripts.items(), key=lambda item: item[1], reverse=True)
-    return [script for script, count in common_scripts]
-
 def user_scripts_from_babel():
     session = any_session()
     languages = session.get(action='query',
                             meta='babel',
                             babuser=identify()['username'])['query']['babel'].keys()
     autonyms = language_autonyms(languages)
-    return scripts_of_text(char for autonym in autonyms.values() for char in autonym)
+    return scripts.scripts_of_text(char for autonym in autonyms.values() for char in autonym)
 
 def language_autonyms(language_codes):
     wikitext = ''
---
+++ scripts.py
@@ -0,0 +1,41 @@
+import bs4
+
+import unicodescripts
+
+
+def scripts_of_text(text):
+    """Determine the scripts used in a snippet of text.
+
+    The 'Common', 'Inherited' and 'Unknown' scripts are ignored."""
+    scripts = {}
+    for char in text:
+        script = unicodescripts.script(char)
+        if script not in {'Common', 'Inherited', 'Unknown'}:
+            scripts[script] = scripts.get(script, 0) + 1
+    common_scripts = sorted(scripts.items(), key=lambda item: item[1], reverse=True)
+    return [script for script, count in common_scripts]
+
+
+def primary_script_of_diff(html):
+    """Determine the primary script of a Wikidata diff.
+
+    Only the scripts of terms, sitelinks, monolingual text values and
+    Commons media are considered."""
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    elements = [content for content in soup.contents if type(content) is bs4.Tag]
+    texts = []
+    for i in range(0, len(elements), 2):
+        lineno = elements[i].get_text()
+        if (lineno.startswith('label / ') or
+            lineno.startswith('description /') or
+            lineno.startswith('aliases /') or
+            lineno.startswith('links /')):
+            texts += (element.get_text() for element in elements[i+1].select('.diff-addedline, .diff-deletedline'))
+        elif lineno.startswith('Property /'):
+            texts += (element.get_text() for element in elements[i+1].select('.wb-monolingualtext-value'))
+            texts += (element.get_text() for element in elements[i+1].select('a.extiw[href^="//commons.wikimedia.org/"]'))
+    scripts = scripts_of_text(char for text in texts for char in text)
+    if scripts:
+        return scripts[0]
+    else:
+        return None
