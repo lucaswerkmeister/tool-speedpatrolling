b6c33a781b8841a6b4be8aac5f1cc46f6646d112
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-02-02 18:18:19 +0100

    Cache revision ID to page ID mappings
    When we retrieve unpatrolled recent changes, MediaWiki already gives us
    the page ID corresponding to each revision ID, so we can store it in a
    cache at that point and then use that cache so we don’t have to look up
    the page ID via the API explicitly. The only case where that API lookup
    is still necessary is when a user manually navigates to /diff/<id>
    without going via the any_diff() endpoint.
    
    A cache size of 1024*1024 elements seems to correspond to an internal
    size of the underlying store of some 84 million bytes, which should be
    totally acceptable on Toolforge.

--- ids.py
+++ ids.py
@@ -1,3 +1,19 @@
+import cachetools
+import threading
+
+
+class MyLRUCache(cachetools.LRUCache):
+    """An LRU cache that does not consider item assignment as use."""
+
+    def __setitem__(self, key, value, cache_setitem=cachetools.Cache.__setitem__):
+        cache_setitem(self, key, value)
+        # no self.__update(key)
+
+
+rev_id_to_page_id_cache = MyLRUCache(maxsize=1024*1024)
+rev_id_to_page_id_cache_lock = threading.RLock()
+
+
 def get(dict, name):
     """Get a list of IDs by name from a container.
 
@@ -17,6 +33,9 @@ def append(dict, name, id):
     dict[name] = ids
 
 
+@cachetools.cached(cache=rev_id_to_page_id_cache,
+                   key=lambda rev_id, session: rev_id,
+                   lock=rev_id_to_page_id_cache_lock)
 def rev_id_to_page_id(rev_id, session):
     return session.get(action='query',
                        revids=[rev_id],
@@ -32,4 +51,6 @@ def unpatrolled_changes(session):
                               rclimit='max',
                               continuation=True):
         for change in result['query']['recentchanges']:
+            with rev_id_to_page_id_cache_lock:
+                rev_id_to_page_id_cache[change['revid']] = change['pageid']
             yield change['revid']
